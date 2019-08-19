cb37cc89db46a730a10402610367e8e9f3c37aa0
baishiyu <baishiyu_i@didiglobal.com>
2019-08-19 19:23:29 +0800

    jq源码进度50%

--- js部分/连等问题.js
+++ js部分/连等问题.js
@@ -2,6 +2,6 @@ var a = {n:1};
 var b = a; // 持有a，以回查
 a.x = a = {n:2};
 console.log(a.x);// --> undefined
-console.log(b.x);// --> {n:2}</span>
+console.log(b.x);// --> {n:2}
 console.log(a);
 console.log(b);
